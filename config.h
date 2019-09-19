5ee00e7936574a433da3b85484e70fbb3143e810
Bakar Chargeishvili <bakar.chargeishvili@gmx.de>
2019-09-19 11:02:12 +0400

    Tmux in default terminal session

--- config.h
+++ config.h
@@ -85,7 +85,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "tmux", "new", NULL };
 static const char *mail[]  = { "st", "-e", "neomutt", NULL };
 static const char *ranger[]  = { "st", "-e", "ranger", NULL };
 static const char *browser[]  = { "qutebrowser", NULL };
